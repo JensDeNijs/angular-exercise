5c5b90f840418ced3a8725f6be0fff477658e543
jens <jensdenijs@hotmail.com>
2021-07-12 16:49:00 +0200

    init

---
+++ friendlist/src/app/friend-form/friend-form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {Friend} from "../friend";
+
+
+@Component({
+  selector: 'app-friend-form',
+  templateUrl: './friend-form.component.html',
+  styleUrls: ['./friend-form.component.css']
+})
+
+export class FriendFormComponent implements OnInit {
+
+  constructor() { }
+
+  public friend = new Friend("","","", "");
+
+  ngOnInit(): void {
+  }
+  data = ["JavaScript","PHP","CSS","HTML"]
+
+}
---
+++ friendlist/src/app/friend.ts
@@ -0,0 +1,14 @@
+export class Friend {
+  fname: string;
+  lname: string;
+  email: string;
+  phone: string;
+
+
+  constructor(fname: string, lname: string, email: string, phone: string) {
+    this.fname = fname;
+    this.lname = lname;
+    this.email = email;
+    this.phone = phone;
+  }
+}
